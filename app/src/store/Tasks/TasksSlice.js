c5c502d8c6c9c0aa11782b231fa0eaa51795ef70
Vybyranyi <mariankvit@gmail.com>
2025-11-09 09:50:51 +0200

    fix API_URL

--- app/src/store/Tasks/TasksSlice.js
+++ app/src/store/Tasks/TasksSlice.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 const initialState = {
   tasks: [],
   searchValue: '',
