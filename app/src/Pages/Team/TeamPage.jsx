5a158d799bd90775a0859fa92ebd3f60cdb7974a
Vybyranyi <mariankvit@gmail.com>
2025-12-01 20:16:20 +0200

    introduce admin-specific user management view on team page and apply Poppins font to user management and password display modal styles.

--- app/src/Pages/Team/TeamPage.jsx
+++ app/src/Pages/Team/TeamPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import TeamList from '../../Components/Team/TeamList/TeamList';
+import UserManagement from '../UserManagement/UserManagement';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import HelmetComponent from '../../Components/Helmet/HelmetComponent';
 
 function TeamPage(props) {
 	const { t } = useTranslation();
 
+	const { userRole } = useSelector(state => state.auth);
+
 	return (
 		<div>
 			<HelmetComponent title={t('helmetTitle.team')} />
-			<TeamList />
+			{userRole === 'admin' ? <UserManagement /> : <TeamList />}
 		</div>
 	);
 }
